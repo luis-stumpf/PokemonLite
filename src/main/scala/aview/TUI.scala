5add944d650dffdb176981e8102a2253bfa7312f
Timothy Hale <timothyh@tutanota.com>
2022-06-26 12:31:34 +0200

    changed welcome message

--- src/main/scala/aview/TUI.scala
+++ src/main/scala/aview/TUI.scala
@@ -49,7 +49,7 @@ class TUI( controller : ControllerInterface ) extends Observer :
 
 
 
-  def welcomeStatement: String = "PokemonLite has loaded, type anything to begin."
+  def welcomeStatement: String = "PokemonLite has started, type anything to begin."
 
   def nameInputRequest : String = "Enter name of Player "+controller.game.turn+": "
 
